4e2b3b3f283b01c1c65c2e937846eedb513b6a92
CHINGUN <chingun@bayaraaEs-iMac.local>
2022-09-28 19:32:46 +0800

    chingun

---
+++ bootstrap/1.js
@@ -0,0 +1,72 @@
+// const users = {
+//     Alex: {
+//       email: ' alex@alex.com ',
+//       skills: ['HTML', 'CSS', 'JavaScript'],
+//       age: 20,
+//       isLoggedIn: false,
+//       points: 30
+//     },
+//     Asab: {
+//       email: ' asab@asab.com ',
+//       skills : ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+//       age: 25,
+//       isLoggedIn: true,
+//     },
+//     Brook: {
+//       email: ' daniel@daniel.com ',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+//       age: 30,
+//       isLoggedIn: true,
+//       points: 50
+//     },
+//     Daniel: {
+//       email: ' daniel@alex.com ',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+//       age: 20,
+//       isLoggedIn: false,
+//       points: 40
+//     },
+//     John: {
+//       email : ' john@john.com ',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+//       age: 20,
+//       isLoggedIn:true,
+//       points: 50
+//     },
+//     Thomas: {
+//       email: 'thomas@thomas.com ',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+//       age: 20,
+//       isLoggedIn: false,
+//       points: 40
+//     },
+//     Paul: {
+//       email: ' paul@paul. com ',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+//       age: 20,
+//       isLoggedIn: false,
+//       points: 40
+//     }
+//   }
+//  let skill = {}
+//  for (let a in users) {
+//      skill += users[a].skills.length;
+//  }
+//  var largest = 0;
+//  for (let i = 0; i < skill.length; i++) {
+//      if (largest < skill[i] ) {
+//          largest = skill[i];
+//      }
+//  }
+//  for (let a in users) {
+//      if (users[a].skills.length == largest) {
+//          console.log(users[a])
+//      }
+//  }
+
+//  const d = [0, 1, 2, 3, 4, 5];
+//  console.log(d[5
+setTimeout (() =>  {
+  console.log("hha");
+}, 1000);
+console.log("hhi");
